46b39d6e15798fc0e7e6756e4b303ca3602319b9
David West <david.b.west@gmail.com>
2013-05-25 10:47:49 -0400

    redirect on signup to user_page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 		user = User.find_by_username(params[:session][:username].downcase)
 		if user && user.authenticate(params[:session][:password])
 			start_session(user)
-			redirect_to root_path
+			redirect_to user_path(user)
 		else
 			redirect_to root_path
 		end
