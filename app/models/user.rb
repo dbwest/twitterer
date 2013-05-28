5060657dfde249f578b2e76139e787f334e43cad
David West <david.b.west@gmail.com>
2013-05-28 14:54:51 -0400

    removed get cookie

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,22 +10,15 @@ class User < ActiveRecord::Base
   # Source for email validation inspiration:
   # http://my.rails-royce.org/2010/07/21/email-validation-in-ruby-on-rails-without-regexp/
   attr_accessible :email, :name, :username, :password, :password_confirmation, :followed_users, :followers
-  has_secure_password
   has_many :tweets, dependent: :destroy
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
   has_many :followed_users, through: :relationships, source: :followed
-  
-
-
-  before_save :get_cookie
 
   validates :name, length: {maximum: 50}
   validates :username, length: {maximum: 15}, uniqueness: true
-  validates :email, email: true
-  validates :password, presence: { on: :create }, length: { minimum: 8 }
-
+  
   def following?(other_user)
     relationships.find_by_followed_id(other_user.id)
   end
@@ -38,9 +31,4 @@ class User < ActiveRecord::Base
     relationships.find_by_followed_id(other_user.id).destroy
   end
 
-  private
-
-  	def get_cookie
-   		self.token = SecureRandom.urlsafe_base64
-   	end
 end
