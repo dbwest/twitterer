7232d06ed2badb8d5574424f0757ebaace823417
David West <david.b.west@gmail.com>
2013-05-28 11:01:35 -0400

    fixed syntax error in tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -7,7 +7,7 @@ describe "UserPages" do
 		let(:user) {FactoryGirl.create(:user)}
 		before do
 			sign_in user
-			visit user_path(user)}
+			visit user_path(user)
 		end
   
 		it { should have_selector('h1', text: user.name)}
